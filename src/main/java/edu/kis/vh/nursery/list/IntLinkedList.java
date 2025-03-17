ff77300e508b26d82d161a86fe5a9c0c5c42fce1
Dariusz Pasiński <245897@edu.p.lodz.pl>
2025-03-17 21:18:24 +0100

    6.1 Dodanie przegapionej stałej

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+    public static final int INTNEGATIVE1 = -1;
     Node last;
     int i;
 
@@ -25,13 +26,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return INTNEGATIVE1;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return INTNEGATIVE1;
         int ret = last.value;
         last = last.prev;
         return ret;
