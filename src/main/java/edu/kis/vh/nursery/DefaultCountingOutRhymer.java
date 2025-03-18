ce0381ca32e1d5883035294129549be84e97a09d
Dariusz Pasiński <245897@edu.p.lodz.pl>
2025-03-18 16:48:11 +0100

    6.2 Popra nazw stałych oraz usunięci stałych z return

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int INT12 = 12;
-    public static final int INTNEGATIVE1 = -1;
-    private final int[] NUMBERS = new int[INT12];
+    public static final int MAXSIZEOFARRAY = 12;
+    public static final int ARRAYISEMPTY = -1;
+    public static final int LASTELEMENTOFARRAY = MAXSIZEOFARRAY - 1;
+    private final int[] NUMBERS = new int[MAXSIZEOFARRAY];
 
-    public int total = INTNEGATIVE1;
+    public int total = ARRAYISEMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == INTNEGATIVE1;
+        return total == ARRAYISEMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == LASTELEMENTOFARRAY;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return INTNEGATIVE1;
+            return -1;
         return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return INTNEGATIVE1;
+            return -1;
         return NUMBERS[total--];
     }
 
