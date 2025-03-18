0e841855df766dea037fbbf6331e2b98d40e9250
Dariusz Pasiński <245897@edu.p.lodz.pl>
2025-03-18 17:22:12 +0100

    6.3 Usunięcie nie potrzebnej stałej.

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,6 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int INTNEGATIVE1 = -1;
     Node last;
     int i;
 
@@ -26,13 +25,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return INTNEGATIVE1;
+            return -1;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return INTNEGATIVE1;
+            return -1;
         int ret = last.value;
         last = last.prev;
         return ret;
